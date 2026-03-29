2736e43f9667f36424e0c46c77041349ea969f37
Michael Wenders <1871010+we-mi@users.noreply.github.com>
2026-03-29 23:28:18 +0200

    Converted to use new "unifi.user" object

--- src/Public/User/Get-UnifiAdmin.ps1
+++ src/Public/User/Get-UnifiAdmin.ps1
@@ -1,71 +1,27 @@
 ﻿function Get-UnifiAdmin {
     <#
     .SYNOPSIS
-        Lists unifi admins for all or just one site
+        List unifi admins
     .DESCRIPTION
-        Lists unifi admins for all or just one site
+        List unifi admins
     .EXAMPLE
-        PS C:\> Get-UnifiAdmin -All
-        Lists unifi admins for all sites
-
-        PS C:\> Get-UnifiAdmin -SiteName "Default"
-        Lists unifi admins for site "Default"
+        PS C:\> Get-UnifiAdmin
+        List unifi admins
     .OUTPUTS
-        Returns JSON-Data
+        Returns an array of 'Unifi.User'
     #>
     [CmdletBinding()]
-    [OutputType([Object])]
+    [OutputType([Unifi.User[]])]
 
-    param(
-        # SiteName
-        [Parameter(Mandatory = $false, ParameterSetName="SiteName", ValueFromPipelineByPropertyName=$True)]
-        [string]
-        $SiteName,
-
-        # Do not filter or rename output, just sent the json result back as raw data
-        [Parameter(Mandatory = $false)]
-        [switch]
-        $Raw,
-
-        # List Admins for all sites
-        [Parameter(Mandatory = $false, ParameterSetName="All")]
-        [switch]
-        $All
-    )
+    param()
 
     process {
-        if (!$All -and [string]::IsNullOrWhiteSpace($SiteName)) {
-            Write-Error "No SiteName was given"
-        } else {
-            if ($All) {
-                $jsonResult = Invoke-UnifiRestCall -Method GET -Route "api/stat/admin"
-            } else {
-                $jsonResult = Invoke-UnifiRestCall -Method POST -Route "api/s/$($SiteName)/cmd/sitemgr" -Body (@{cmd = "get-admins"} | ConvertTo-JSON)
-            }
 
-            if ($jsonResult.meta.rc -eq "ok") {
-                if ($Raw) {
-                    $jsonResult.data
-                } else {
-                    if ($All) {
-                        $jsonResult.data | Select-Object    name,email,
-                                                        @{N="UserID";E={$_._id}},
-                                                        @{N="SuperAdmin";E={$_.is_super}},
-                                                        @{N="Roles";E={$_.roles}},
-                                                        @{N="SuperRoles";E={$_.super_roles}},
-                                                        @{N="CreatedOn";E={ ( Get-Date('1970-01-01 00:00:00') ).AddSeconds($_.time_created) }},
-                                                        @{N="LastSiteName";E={$_.last_site_name}},
-                                                        @{N="EMailAlert";E={$_.email_alert_enabled}}
-                    } else {
-                        $jsonResult.data | Select-Object    name,email,
-                                                        @{N="UserID";E={$_._id}},
-                                                        @{N="Permissions";E={$_.permissions}},
-                                                        @{N="SuperAdmin";E={$_.is_super}},
-                                                        @{N="Role";E={$_.role}},
-                                                        @{N="EMailAlert";E={$_.email_alert_enabled}}
-                    }
-                }
-            }
+        $jsonResult = Invoke-UnifiRestCall -Method GET -Route "stat/admin"
+
+        foreach ($user in $jsonResult) {
+            [Unifi.User]::new($user)
         }
+        
     }
 }
--- src/Public/User/Get-UnifiLogin.ps1
+++ src/Public/User/Get-UnifiLogin.ps1
@@ -8,27 +8,16 @@
         PS C:\> Get-UnifiLogin
         Shows information about the currently logged in user
     .OUTPUTS
-        Returns JSON-Data
+        Returns an object of type 'Unifi.User'
     #>
     [CmdletBinding()]
-    [OutputType([Object])]
+    [OutputType([Unifi.User])]
 
-    param(
-        # Do not filter or rename output, just sent the json result back as raw data
-        [Parameter(Mandatory = $false)]
-        [switch]
-        $Raw
-    )
+    param()
 
     process {
-        $jsonResult = Invoke-UnifiRestCall -Method GET -Route "api/self"
-
-        if ($jsonResult.meta.rc -eq "ok") {
-            if ($Raw) {
-                $jsonResult.data
-            } else {
-                $jsonResult.data | Select-Object Name,@{N="AdminID";E={$_.admin_id}},EMail,@{N="EMailAlert";E={$_.email_alert_enabled}},@{N="SuperAdmin";E={$_.is_super}},@{N="UISettings";E={$_.ui_settings}}
-            }
-        }
+        $jsonResult = Invoke-UnifiRestCall -Method GET -Route "self"
+        
+        [Unifi.User]::new($jsonResult)
     }
 }
