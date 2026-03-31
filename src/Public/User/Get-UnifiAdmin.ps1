3f27c6b4f85f01f7a6e570fa2581eb9a3a5810a5
Michael Wenders <1871010+we-mi@users.noreply.github.com>
2026-03-31 22:50:09 +0200

    Added simple filters

--- src/Public/User/Get-UnifiAdmin.ps1
+++ src/Public/User/Get-UnifiAdmin.ps1
@@ -7,21 +7,38 @@
     .EXAMPLE
         PS C:\> Get-UnifiAdmin
         List unifi admins
+    .EXAMPLE
+        PS C:\> Get-UnifiAdmin -Name *admin*
+        List all unifiadmins which have "admin" in their names
     .OUTPUTS
         Returns an array of 'Unifi.User'
     #>
     [CmdletBinding()]
     [OutputType([Unifi.User[]])]
 
-    param()
+    param(
+        [Parameter()]
+        [String]$Name,
+
+        [Parameter()]
+        [String]$Email
+    )
 
     process {
 
         $jsonResult = Invoke-UnifiRestCall -Method GET -Route "stat/admin"
 
+        if ( ![String]::IsNullOrWhiteSpace($Name) ) {
+            $jsonResult = $jsonResult | Where-Object { $_.Name -like $Name }
+        }
+
+        if ( ![String]::IsNullOrWhiteSpace($Email) ) {
+            $jsonResult = $jsonResult | Where-Object { $_.email -like $Email }
+        }
+
         foreach ($user in $jsonResult) {
             [Unifi.User]::new($user)
         }
-        
+
     }
 }
