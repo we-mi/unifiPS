bc066d90bd11c3e87650c39cfefda335b11dfdbc
Michael Wenders <1871010+we-mi@users.noreply.github.com>
2026-04-06 20:52:22 +0200

    Adapted to new Invoke-UnifiRestCall method (will remove this later as this is implemented in Get-UnifiSite)

--- src/Public/Site/Get-UnifiSiteInfo.ps1
+++ src/Public/Site/Get-UnifiSiteInfo.ps1
@@ -38,89 +38,86 @@
     process {
 
         try {
-            $jsonResult = Invoke-UnifiRestCall -Method GET -Route "api/s/$($SiteName)/stat/health"
+            $jsonResult = Invoke-UnifiRestCall -Method GET -Route "s/$($SiteName)/stat/health"
 
-            if ($jsonResult.meta.rc -eq "ok") {
+            if ($Raw) {
+                $jsonResult
+            } else {
+                foreach ($subsystem in $jsonResult) {
+                    switch ($subsystem.subsystem) {
+                        "wlan" {
+                            $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
+                                                        Subsystem, Status,
+                                                        @{N="APs";E={$_.num_ap}},
+                                                        @{N="Adopted";E={$_.num_adopted}},
+                                                        @{N="Disabled";E={$_.num_disabled}},
+                                                        @{N="Disconnected";E={$_.num_disconnected}},
+                                                        @{N="Pending";E={$_.num_pending}},
+                                                        @{N="Users";E={$_.num_user}},
+                                                        @{N="Guests";E={$_.num_guest}},
+                                                        @{N="IOT";E={$_.num_iot}},
+                                                        @{N="TX";E={$_."tx_bytes-r"}},
+                                                        @{N="RX";E={$_."rx_bytes-r"}}
+                        }
 
-                if ($Raw) {
-                    $jsonResult.data
-                } else {
-                    foreach ($subsystem in $jsonResult.data) {
-                        switch ($subsystem.subsystem) {
-                            "wlan" {
-                                $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
-                                                            Subsystem, Status,
-                                                            @{N="APs";E={$_.num_ap}},
-                                                            @{N="Adopted";E={$_.num_adopted}},
-                                                            @{N="Disabled";E={$_.num_disabled}},
-                                                            @{N="Disconnected";E={$_.num_disconnected}},
-                                                            @{N="Pending";E={$_.num_pending}},
-                                                            @{N="Users";E={$_.num_user}},
-                                                            @{N="Guests";E={$_.num_guest}},
-                                                            @{N="IOT";E={$_.num_iot}},
-                                                            @{N="TX";E={$_."tx_bytes-r"}},
-                                                            @{N="RX";E={$_."rx_bytes-r"}}
-                            }
+                        "wan" {
+                            $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
+                                                        Subsystem, Status,
+                                                        @{N="Gateways";E={$_.num_gw}},
+                                                        @{N="Adopted";E={$_.num_adopted}},
+                                                        @{N="Disconnected";E={$_.num_disconnected}},
+                                                        @{N="Pending";E={$_.num_pending}},
+                                                        @{N="IP";E={$_.wan_ip}},
+                                                        @{N="Gateway";E={$_.gateways}},
+                                                        @{N="Netmask";E={$_.netmask}},
+                                                        @{N="Nameservers";E={$_.nameservers}},
+                                                        @{N="MAC";E={$_.gw_mac}},
+                                                        @{N="Name";E={$_.gw_name}},
+                                                        @{N="Version";E={$_.gw_version}},
+                                                        @{N="Uptime";E={$_.uptime_stats}},
+                                                        @{N="Stats";E={$_.'gw_system-stats'}},
+                                                        @{N="TX";E={$_."tx_bytes-r"}},
+                                                        @{N="RX";E={$_."rx_bytes-r"}},
+                                                        @{N="STA";E={$_."num_sta"}}
+                        }
 
-                            "wan" {
-                                $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
-                                                            Subsystem, Status,
-                                                            @{N="Gateways";E={$_.num_gw}},
-                                                            @{N="Adopted";E={$_.num_adopted}},
-                                                            @{N="Disconnected";E={$_.num_disconnected}},
-                                                            @{N="Pending";E={$_.num_pending}},
-                                                            @{N="IP";E={$_.wan_ip}},
-                                                            @{N="Gateway";E={$_.gateways}},
-                                                            @{N="Netmask";E={$_.netmask}},
-                                                            @{N="Nameservers";E={$_.nameservers}},
-                                                            @{N="MAC";E={$_.gw_mac}},
-                                                            @{N="Name";E={$_.gw_name}},
-                                                            @{N="Version";E={$_.gw_version}},
-                                                            @{N="Uptime";E={$_.uptime_stats}},
-                                                            @{N="Stats";E={$_.'gw_system-stats'}},
-                                                            @{N="TX";E={$_."tx_bytes-r"}},
-                                                            @{N="RX";E={$_."rx_bytes-r"}},
-                                                            @{N="STA";E={$_."num_sta"}}
-                            }
+                        "www" {
+                            $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
+                                                        Subsystem, Status,
+                                                        @{N="TX";E={$_."tx_bytes-r"}},
+                                                        @{N="RX";E={$_."rx_bytes-r"}},
+                                                        @{N="Latency";E={$_.latency}},
+                                                        @{N="Uptime";E={$_.Uptime}},
+                                                        @{N="Drops";E={$_.Drops}},
+                                                        @{N="Up";E={$_.xput_up}},
+                                                        @{N="Down";E={$_.xput_down}},
+                                                        @{N="SpeedtestStatus";E={$_.speedtest_status}},
+                                                        @{N="SpeedtestLastRun";E={$_.speedtest_lastrun}},
+                                                        @{N="SpeedtestPing";E={$_.speedtest_ping}},
+                                                        @{N="MAC";E={$_.gw_mac}}
+                        }
 
-                            "www" {
-                                $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
-                                                            Subsystem, Status,
-                                                            @{N="TX";E={$_."tx_bytes-r"}},
-                                                            @{N="RX";E={$_."rx_bytes-r"}},
-                                                            @{N="Latency";E={$_.latency}},
-                                                            @{N="Uptime";E={$_.Uptime}},
-                                                            @{N="Drops";E={$_.Drops}},
-                                                            @{N="Up";E={$_.xput_up}},
-                                                            @{N="Down";E={$_.xput_down}},
-                                                            @{N="SpeedtestStatus";E={$_.speedtest_status}},
-                                                            @{N="SpeedtestLastRun";E={$_.speedtest_lastrun}},
-                                                            @{N="SpeedtestPing";E={$_.speedtest_ping}},
-                                                            @{N="MAC";E={$_.gw_mac}}
-                            }
+                        "lan" {
+                            $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
+                                                        Subsystem, Status,
+                                                        @{N="Users";E={$_.num_user}},
+                                                        @{N="Guests";E={$_.num_guest}},
+                                                        @{N="IOT";E={$_.num_iot}},
+                                                        @{N="TX";E={$_."tx_bytes-r"}},
+                                                        @{N="RX";E={$_."rx_bytes-r"}},
+                                                        @{N="Switche";E={$_.num_sw}},
+                                                        @{N="Adopted";E={$_.num_adopted}},
+                                                        @{N="Disconnected";E={$_.num_disconnected}},
+                                                        @{N="Pending";E={$_.num_pending}}
+                        }
 
-                            "lan" {
-                                $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
-                                                            Subsystem, Status,
-                                                            @{N="Users";E={$_.num_user}},
-                                                            @{N="Guests";E={$_.num_guest}},
-                                                            @{N="IOT";E={$_.num_iot}},
-                                                            @{N="TX";E={$_."tx_bytes-r"}},
-                                                            @{N="RX";E={$_."rx_bytes-r"}},
-                                                            @{N="Switche";E={$_.num_sw}},
-                                                            @{N="Adopted";E={$_.num_adopted}},
-                                                            @{N="Disconnected";E={$_.num_disconnected}},
-                                                            @{N="Pending";E={$_.num_pending}}
-                            }
-
-                            "vpn" {
-                                $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
-                                                            Subsystem, Status
-                            }
+                        "vpn" {
+                            $subsystem | Select-Object  @{N="SiteName";E={$SiteName}},
+                                                        Subsystem, Status
                         }
                     }
-
                 }
+
             }
 
         } catch {
